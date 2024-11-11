5eb9936b7933d2f6684a4faa5d48e19abf55a024
SantiagoJurgelenas <135331447+SantiagoJurgelenas@users.noreply.github.com>
2024-11-11 15:50:14 -0300

    Footer y Navbar momentáneos
    quizás no tan momentáneos.

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>&copy; 2024 El Negro Jacobo | Developed by Jurge alias: El Negro Jacobo</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h1>El Negro Jacobo</h1>
+            <a href="https://deckofcardsapi.com/?authuser=0" target="_blank" rel="noopener noreferrer">API</a>
+        </div>
+    );
+};
+
+export default Navbar;
